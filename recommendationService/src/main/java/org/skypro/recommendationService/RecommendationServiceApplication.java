0942b3b2cdc084484c3e9398a9eb99761ba012cf
Lada <ladasergeevna@mail.ru>
2025-06-13 12:16:53 +0900

    Initial commit

---
+++ recommendationService/src/main/java/org/skypro/recommendationService/RecommendationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.recommendationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommendationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommendationServiceApplication.class, args);
+	}
+
+}
